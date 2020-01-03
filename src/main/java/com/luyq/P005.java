04749e39de9c127ff9be5a87fa57e50bc4bea4b0
John Lu <john.lu01@sap.com>
2020-01-03 15:57:48 +0800

    add problem 5

---
+++ src/main/java/com/luyq/P005.java
@@ -0,0 +1,43 @@
+package com.luyq;
+
+/**
+ * 5. 最长回文子串 (Medium)
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 示例：
+ * Input: "babad"
+ * Output: "bab" or "aba"
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+public class P005 {
+    public static void main(String[] args) {
+        Solution5_1 s1 = new Solution5_1();
+        System.out.println(s1.longestPalindrome("cbbd"));
+    }
+}
+
+// 中心扩展算法: 我们观察到回文中心的两侧互为镜像。因此，回文可以从它的中心展开，并且只有 2n - 12n−1 个这样的中心。
+class Solution5_1 {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
